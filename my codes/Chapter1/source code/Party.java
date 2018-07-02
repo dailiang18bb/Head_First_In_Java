58941818883b31059271a039872a89c1b7ce3e89
Charles <dailiang18bb@gmail.com>
2018-07-01 21:46:15 -0400

    Chap01

---
+++ my codes/Chapter1/source code/Party.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+class Party{
+	public void buildInvite(){
+	Frame f = new Frame();
+	Label i = new Label("Party at Tim's");
+	Button b = new Button("you bet");
+	Button c = new Button("Shoot me");
+	Panel p = new Panel();
+	p.add(i);
+	} // more code here...
+}
