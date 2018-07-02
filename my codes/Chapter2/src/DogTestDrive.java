422fb4653c81db628cef600e4af57e7ae8b98beb
Charles <dailiang18bb@gmail.com>
2018-07-02 18:55:50 -0400

    chap2

---
+++ my codes/Chapter2/src/DogTestDrive.java
@@ -0,0 +1,21 @@
+
+class DogTestDrive{
+	public static void main(String args[]){
+		Dog d = new Dog();
+		d.size = 40;
+		d.bark();
+	}
+}
+
+
+class Dog {
+	
+	int size;
+	String breed;
+	String name;
+
+	void bark(){
+		System.out.println("Ruff! Ruff!");
+	}
+}
+
---
+++ my codes/Chapter2/src/DrumKitTestDrive.java
@@ -0,0 +1,32 @@
+class DrumKitTestDrive{
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		d.playSnare();
+
+		d.snare = false;
+		if (d.snare == true){
+			d.playSnare();
+		}
+		d.playTopHat();
+
+
+		
+	}
+}
+
+
+class DrumKit{
+
+	boolean topHat = true;
+	boolean snare = true;
+
+	void playSnare(){
+		System.out.println("bang bang ba-bang");
+	}
+
+	void playTopHat(){
+		System.out.println("ding ding da-ding");
+	}
+
+}
+
