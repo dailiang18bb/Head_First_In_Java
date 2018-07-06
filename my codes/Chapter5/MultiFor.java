62aa4e6362eefbf75f42c4fe49d7b16650fa0a80
Charles <dailiang18bb@gmail.com>
2018-07-06 12:13:07 -0400

    chapter5 finished

---
+++ my codes/Chapter5/MultiFor.java
@@ -0,0 +1,29 @@
+class MultiFor{
+	public static void main(String[] args) {
+		for(int x = 0; x < 4; x++){
+			for(int y= 4;y>2;y--){
+				System.out.println(x + "" + y);
+			}
+			if(x == 1){
+				x++;
+			}
+		}
+	}
+}
+
+
+x = x + 3; // 54 6
+
+x = x + 6; // 60 10
+
+
+x = x + 2; // 45 6
+
+
+x++; // 36 6
+
+
+x--; // 18 6
+
+x = x + 0; // 6 14
+
